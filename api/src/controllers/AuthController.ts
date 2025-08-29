5dd889698585d6a7f77758d41110299ce65c5013
samuelalex93 <smk.tecinfo@gmail.com>
2025-08-29 19:38:26 -0300

    refactor: login route

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,8 +1,8 @@
 import { Controller, Route, Post, Body, Tags } from "tsoa";
 import { AuthService } from "../services/AuthService";
 import { LoginDto, TokenResponse } from "../types/AuthTypes";
-@Route("auth")
-@Tags("Auth")
+@Route("login")
+@Tags("Login")
 export class AuthController extends Controller {
   private authService: AuthService;
 
@@ -11,7 +11,7 @@ export class AuthController extends Controller {
     this.authService = new AuthService();
   }
 
-  @Post("login")
+  @Post()
   public async login(@Body() body: LoginDto): Promise<TokenResponse> {
     const {email, password} = body
     const result = await this.authService.login(email, password);
